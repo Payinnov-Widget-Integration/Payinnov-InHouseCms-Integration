5f0aa34d362802e63827a4217189b610dfe7aa77
Cirrus-8691 <52166657+Cirrus-8691@users.noreply.github.com>
2023-07-31 10:38:01 +0200

    Add files via upload

---
+++ src/Ts/signature.ts
@@ -0,0 +1,24 @@
+// https://nodejs.org/api/crypto.html
+import crypto from 'crypto';
+/**
+ * Compute headers.signature of an HTTPS request
+ * @param retailerSecretKey retailer Secret API Key 
+ * @param endpoint called sample: "/api/v1/pay/Order"
+ * @param payload  body of POST request
+ * @returns signature
+ */
+function signature(retailerSecretKey: string, endpoint: string, payload: unknown): string {
+    // Unix timestamp convert to string : https://www.unixtimestamp.com/
+    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+    const time = Date.now().toString(); 
+    const signature = crypto.createHmac('sha256', retailerSecretKey);
+    const data = {
+        date: time,
+        method: 'POST',
+        endpoint,
+        payload
+    };
+    const jsonData = JSON.stringify(data);
+    signature.update( jsonData );
+    return `${time}:${signature.digest('base64')}`;
+}
